4936108b87cc5fe93b4c5dd568ecc66a1befd327
ZakharDay <zakhar.day@gmail.com>
2023-02-03 11:48:19 +0300

    Simple timeout

---
+++ src/adcgame.js
@@ -0,0 +1,37 @@
+import './adcgame.css'
+
+console.error('=====================')
+//
+//
+//
+
+const messages = [
+  'Ghbdtn! Rfr ltkf?',
+  'Ой',
+  'Привет! Как дела?',
+  'Ладно, знаешь меня?'
+]
+
+function showMessage(message) {
+  const element = document.createElement('div')
+  element.innerText = message
+  element.classList.add('message')
+
+  document.body.appendChild(element)
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  showMessage(messages[0])
+
+  setTimeout(() => {
+    showMessage(messages[1])
+  }, 2000)
+
+  setTimeout(() => {
+    showMessage(messages[2])
+  }, 4000)
+
+  setTimeout(() => {
+    showMessage(messages[3])
+  }, 6000)
+})
