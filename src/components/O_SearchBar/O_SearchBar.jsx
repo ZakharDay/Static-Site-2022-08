7bf3c4fcd035e1d2f85b24f09014d94c5bc0281f
ZakharDay <zakhar.day@gmail.com>
2023-04-25 17:46:17 +0300

    Search form

--- src/components/O_SearchBar/O_SearchBar.jsx
+++ src/components/O_SearchBar/O_SearchBar.jsx
@@ -1,13 +1,41 @@
 import './O_SearchBar.scss'
 import React from 'react'
+import { getPostTeasers } from '../../search_data.js'
 
 import M_SearchForm from '../M_SearchForm/M_SearchForm.jsx'
 
 export default class O_SearchBar extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      postTeasers: []
+    }
+  }
+
+  componentDidMount() {
+    getPostTeasers().then((data) => {
+      this.setState({
+        postTeasers: data
+      })
+    })
+  }
+
   render() {
+    console.log('Render', this.state.postTeasers)
+
+    const { postTeasers } = this.state
+
+    const posts = []
+
+    postTeasers.forEach((teaser) => {
+      posts.push(<div>{teaser.title}</div>)
+    })
+
     return (
       <div className="O_SearchBar">
         <M_SearchForm />
+        {posts}
       </div>
     )
   }
